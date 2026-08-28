f3e09da3132c36a2e77c668c6fabfbc7a1c5b65f
Gabriel <gabrielnodejs@gmail.com>
2026-08-27 18:57:20 -0600

    Configuracion de settings.py para archivos estaticos

--- config/settings.py
+++ config/settings.py
@@ -124,10 +124,8 @@ USE_TZ = True
 
 # Archivos estaticos
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
-
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Directorio media
 
